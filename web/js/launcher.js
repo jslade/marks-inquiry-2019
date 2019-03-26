6fc14c2886bbbf71291226da96ac22a3d95f5bcc
Jeremy Slade <jeremy@jkslade.net>
2019-03-25 19:42:47 -0600

    Start of launcher site

---
+++ web/js/launcher.js
@@ -0,0 +1,15 @@
+var showJoinCode = function() {
+  $('#instructions h1').text('Scan the QR code to join the next game');
+
+  var link =
+    window.location.protocol + "//" +
+    window.location.hostname +
+    "/controller.html"
+
+  new QRCode($('#qrcode')[0], link);
+};
+
+
+$(document).ready(function () {
+  showJoinCode();
+});
