08b47cf0aa48652740abefee6cb7deece57dc7aa
mark slade <mark@jkslade.net>
2019-03-27 17:29:45 -0600

    Added 2 more buttons to the start scene

--- snake/start_scene.py
+++ snake/start_scene.py
@@ -4,19 +4,21 @@ from .engine.game_scene import GameScene
 from .engine.layer import Layer
 
 class StartScene(GameScene):
+    def on_activated(self, screen):
+        button_layer = Layer(mousable=True)
+        self.add_layer(button_layer)
 
-
-    button_layer = Layer(mousable=True)
-        button = Button(
-            text='Click me!',
-            action=self.button_clicked,
-            color=(1,128,1),
-            hover_color=(0,200,0),
-            text_color=(255,255,255)
-        )
+        button = Button(text='Click me!', action=self.button_clicked, color=(1,128,1), hover_color=(0,200,0), text_color=(255,00,00))
         button.rect = self.Rect(100,100,100,40)
         button_layer.add_object(button)
-        self.add_layer(button_layer)
+
+        button = Button(text='Help', action=self.button_clicked, color=(1,128,1), hover_color=(0,200,0), text_color=(00,255,00))
+        button.rect = self.Rect(100,200,110,40)
+        button_layer.add_object(button)
+
+        button = Button(text='Exit', action=self.button_clicked, color=(1,128,1), hover_color=(0,200,0), text_color=(00,00,255))
+        button.rect = self.Rect(100,300,100,40)
+        button_layer.add_object(button)
 
     def button_clicked(self, event):
         self.log("Yay!")
