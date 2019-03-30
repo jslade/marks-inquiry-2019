c456e615367f6c2ceb9fa65f2854927f43a5b1a0
Jeremy Slade <jeremy@jkslade.net>
2019-03-29 20:10:29 -0600

    Fixed food rendering scale and hitbox

--- snake/food.py
+++ snake/food.py
@@ -6,19 +6,24 @@ from .snake import Snake
 
 
 class Food(BoundedObject):
-    def __init__(self, ):
+    def __init__(self, scale=2.0, visual_scale=1.2):
         BoundedObject.__init__(self)
+
+        self.scale = scale
+        self.visual_scale = visual_scale
 
         self.color = (180, 20, 40)
         self.color2 = Colors.darker(self.color, .50)
 
-        self.rect.width = Settings.snake_size
-        self.rect.height = Settings.snake_size
+        self.rect.width = int(Settings.snake_size * scale)
+        self.rect.height = int(Settings.snake_size * scale)
 
         self.prerender()
 
     def prerender(self):
-        visual_size = int(Settings.snake_size * 1.2)
+        visual_size = int(Settings.snake_size * self.scale * self.visual_scale)
+        dv = int((visual_size - self.rect.width) / 2)
+
         self.surface = self.Surface(
             (visual_size, visual_size),
             flags=self.pygame.SRCALPHA
@@ -27,16 +32,17 @@ class Food(BoundedObject):
         self.draw.circle(
             self.surface,
             self.color2,
-            (int(visual_size / 2), int(visual_size / 2)),
+            (int(visual_size / 2) - dv, int(visual_size / 2) - dv),
             int(visual_size / 2)
         )
         self.draw.circle(
             self.surface,
             self.color,
-            (int(visual_size / 2), int(visual_size / 2)),
+            (int(visual_size / 2) - dv, int(visual_size / 2) - dv),
             int(visual_size / 2.5)
         )
 
-
     def render(self, surface):
         surface.blit(self.surface, self.rect.topleft)
+        self.draw.rect(surface, (0,255,0), self.rect, 2)
+
